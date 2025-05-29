c7b9889678f1d2e929e5840667054d34a024f8d3
CSelenetvauri <elene.tvauri123@gmail.com>
2025-05-29 09:40:49 +0400

    Add files via upload

---
+++ t2/Student.java
@@ -0,0 +1,29 @@
+package oop.mid2.t2;
+
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<LearningCourse> learningCourses;
+
+    public Student(String name, List<LearningCourse> learningCourses) {
+        this.name = name;
+        this.learningCourses = learningCourses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<LearningCourse> getLearningCourses() {
+        return learningCourses;
+    }
+
+    public void setLearningCourses(List<LearningCourse> learningCourses) {
+        this.learningCourses = learningCourses;
+    }
+}
